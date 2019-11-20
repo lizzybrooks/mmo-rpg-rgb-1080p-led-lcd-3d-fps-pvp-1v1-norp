c0e00ab87d4ae7c4a29dfcb400566eba64770d02
lengurd <jscherr001@gmail.com>
2019-11-20 13:13:36 -0800

    bruh

--- 5-animated-sprites/sketch.js
+++ 5-animated-sprites/sketch.js
@@ -1,35 +1,48 @@
-var xMove = 0;
-var xMoveN = 0;
-var yMove = 0;
-var yMoveN = 0;
+
 let char;
 let groundTest
 let headTest
 
 function setup() {
   createCanvas(1600, 900);
-  char = new bruh(800, 800,"white");
+  char = new bruh(800, 800, 0, 0, 0, 0, false, "white");
+  ground = new platform(0, 830, 1600, 100)
+  plat1 = new platform(400, 700, 150, 10)
 }
 
 function draw() {
   noStroke()
   background(10, 163, 240);
   fill(148, 240, 10)
-  rect(0, 830, 1600, 100);
-  rect (400, 700, 150, 10)
+  // rect(0, 830, 1600, 100);
+  // rect (400, 700, 150, 10)
+  rect (600, 570, 150, 10)
   fill(110, 86, 8)
   rect(400, 710, 150, 7)
-  groundTest = get(char.x, char.y+31)
+  rect(600, 580, 150, 7)
+  groundTest = get(char.x, char.y+32)
   headTest = get(char.x, char.y)
   char.drawBruh();
   char.moveBruh();
+  char.land()
+  fill(148, 240, 10)
+  ground.drawPlatform();
+  plat1.drawPlatform();
+
+
 }
 
 class bruh {
-	constructor(x,y,color){
+	constructor(x,y,xMove, xMoveN, yMove, yMoveN, jumpable, color){
 	   this.x = x;
      this.y = y;
+     this.xMove = xMove
+     this.xMoveN = xMoveN
+     this.yMove = yMove
+     this.yMoveN = yMoveN
      this.color= color;
+     this.jumpable = jumpable
+
 	}
 
 	drawBruh(){
@@ -39,53 +52,53 @@ class bruh {
 	}
 
 	moveBruh(){
-    this.x=this.x+xMove+xMoveN
-    this.y=this.y+yMove+yMoveN
-    yMoveN+=.4
+    this.x=this.x+this.xMove+this.xMoveN
+    this.y=this.y+this.yMove+this.yMoveN
+    this.yMoveN+=.4
 
-    if (xMove < 0){
-      xMove= 0
+    if (this.xMove < 0){
+      this.xMove= 0
     }
 
-    if (xMove > 0){
-      xMove-=0.2
+    if (this.xMove > 0){
+      this.xMove-=0.2
     }
 
-    if (xMoveN > 0){
-      xMoveN= 0
+    if (this.xMoveN > 0){
+      this.xMoveN= 0
     }
 
-    if (xMoveN < 0){
-      xMoveN+=0.2
+    if (this.xMoveN < 0){
+      this.xMoveN+=0.2
     }
 
-    if (groundTest[0] == 148 && groundTest[1] == 240 && groundTest[2] == 10){
-      yMoveN=0
-      this.y=this.y-0.4
-    }
+    // if (groundTest[0] == 148 && groundTest[1] == 240 && groundTest[2] == 10){
+    //   this.yMoveN=0
+    //   this.y=this.y-0.4
+    //}
 
     if (keyIsDown(RIGHT_ARROW)){
-      xMove=xMove+3
+      this.xMove=this.xMove+3
     }
 
-    if (xMove > 7){
-      xMove=7
+    if (this.xMove > 7){
+      this.xMove=7
     }
 
     if (keyIsDown(LEFT_ARROW)){
-      xMoveN=xMoveN-3
+      this.xMoveN=this.xMoveN-3
     }
 
-    if (xMoveN < -7){
-      xMoveN=-7
+    if (this.xMoveN < -7){
+      this.xMoveN=-7
     }
 
     if (headTest[0] == 110 && headTest[1] == 86 && headTest[2] == 8){
-      yMoveN= .3
+      this.yMoveN= .3
     }
 
-    if (keyIsDown(UP_ARROW) && groundTest[0] == 148 && groundTest[1] == 240 && groundTest[2] == 10){
-      yMoveN=yMoveN-10
+    if (keyIsDown(UP_ARROW) && this.jumpable == true){
+      this.yMoveN=this.yMoveN-3
     }
 
     if (this.x >1570){
@@ -96,4 +109,26 @@ class bruh {
       this.x=0
     }
 	}
+  land(){
+    if(this.y>800){
+      this.yMoveN=0
+      this.y=this.y-0.4
+      print("landed")
+      this.jumpable = true
+
+    }
+
+  }
+}
+
+class platform {
+  constructor(x,y,w,h){
+    this.x = x;
+    this.y = y;
+    this.w = w;
+    this.h = h
+  }
+  drawPlatform(){
+    rect(this.x, this.y, this.w, this.h)
+  }
 }
